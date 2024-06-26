33085d3891c939c33ae383b00db9048b0e20bc23
Xian Zheng <seeinplay@gmail.com>
2024-06-25 22:07:18 -0600

    purple background and white text

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -82,6 +82,9 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      backgroundImage: {
+        "custom-purple-gradient": "linear-gradient(45deg, #251b3a, #8731c1)",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
