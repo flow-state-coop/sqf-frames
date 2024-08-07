9ad7c43e7f26724d2f4dceee2d240edf3bc6cc62
Xian Zheng <seeinplay@gmail.com>
2024-08-07 15:53:47 -0600

    use abritrum ETHx and USDCx

--- app/frames/constants.ts
+++ app/frames/constants.ts
@@ -1,51 +1,51 @@
-// constants.ts
-
 export const chainConfig: Record<
-  string,
+  `0x${string}`,
   {
     cfaForwarderAddress: `0x${string}`;
-    addressX: `0x${string}`;
+    chainId: string;
     name: string;
     chainName: string;
     rpcUrl?: string;
   }
 > = {
-  // degenx
-  "666666666": {
+  "0xda58FA9bfc3D3960df33ddD8D4d762Cf8Fa6F7ad": {
     cfaForwarderAddress: "0xcfA132E353cB4E398080B9700609bb008eceB125",
-    addressX: "0xda58FA9bfc3D3960df33ddD8D4d762Cf8Fa6F7ad",
+    chainId: "666666666",
     name: "DEGEN",
     chainName: "degenchain",
     rpcUrl: "https://rpc.degen.tips",
   },
-  // celox
-  "42220": {
+  "0x671425Ae1f272Bc6F79beC3ed5C4b00e9c628240": {
     cfaForwarderAddress: "0xcfA132E353cB4E398080B9700609bb008eceB125",
-    addressX: "0x671425Ae1f272Bc6F79beC3ed5C4b00e9c628240",
+    chainId: "42220",
     name: "CELO",
     chainName: "Celo",
     rpcUrl: "https://forno.celo.org",
   },
-  // arbitrum usdcx
-  "42161": {
+  "0xFc55F2854e74b4f42D01a6d3DAAC4c52D9dfdcFf": {
     cfaForwarderAddress: "0xcfA132E353cB4E398080B9700609bb008eceB125",
-    addressX: "0xFc55F2854e74b4f42D01a6d3DAAC4c52D9dfdcFf",
-    name: "Arbitrum USDC",
+    chainId: "42161",
+    name: "Arbitrum USDCx",
     chainName: "Arbitrum One",
     rpcUrl: "https://arb1.arbitrum.io/rpc",
   },
-  // base
-  "31337": {
+  "0xe6C8d111337D0052b9D88BF5d7D55B7f8385ACd3": {
     cfaForwarderAddress: "0xcfA132E353cB4E398080B9700609bb008eceB125",
-    addressX: "0xefbE11336b0008dCE3797C515E6457cC4841645c",
+    chainId: "42161",
+    name: "Arbitrum ETHx",
+    chainName: "Arbitrum One",
+    rpcUrl: "https://arb1.arbitrum.io/rpc",
+  },
+  "0xefbE11336b0008dCE3797C515E6457cC4841645c": {
+    cfaForwarderAddress: "0xcfA132E353cB4E398080B9700609bb008eceB125",
+    chainId: "31337",
     name: "Base",
     chainName: "base-mainnet",
     rpcUrl: "https://mainnet.base.org",
   },
-  // optimism sepolia
-  "11155420": {
+  "0x0043d7c85C8b96a49A72A92C0B48CdC4720437d7": {
     cfaForwarderAddress: "0xcfA132E353cB4E398080B9700609bb008eceB125",
-    addressX: "0x0043d7c85C8b96a49A72A92C0B48CdC4720437d7",
+    chainId: "11155420",
     name: "Optimism Sepolia",
     chainName: "optimism-sepolia",
     rpcUrl: "https://sepolia.optimism.io",
